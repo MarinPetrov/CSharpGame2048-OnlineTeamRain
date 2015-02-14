4e90a5dc016bd3c8eeb99dae1042e8d1a2bc80a0
MarinPetrov <marinpetrov02@gmail.com>
2015-02-15 00:48:54 +0200

    Start Project

---
+++ GameTeamRain/GameTeamRain/GameCore.cs
@@ -0,0 +1,14 @@
+﻿namespace GameTeamRain
+{
+    using System;
+    using System.Collections.Generic;
+    class GameCore
+    {
+        private ushort[,] coreMatrix;
+        private Random randomNumber = new Random();
+        public GameCore()
+        {
+            this.coreMatrix = new ushort[4, 4];
+        }
+    }
+}
---
+++ GameTeamRain/GameTeamRain/GameTeamRainLogic.cs
@@ -0,0 +1,13 @@
+﻿namespace GameTeamRain
+{
+    using System;
+    using System.Collections.Generic;
+    class GameTeamRainLogic
+    {
+        static void Main()
+        {
+            GameCore engine = new GameCore();
+            
+        }
+    }
+}
