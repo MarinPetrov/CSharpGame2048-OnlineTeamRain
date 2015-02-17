01211143d3d939be8269bf3f02c1865bc75a7622
MarinPetrov <marinpetrov02@gmail.com>
2015-02-17 14:22:36 +0200

    Implement public bool ReCalculateMatrix() public bool CalculateDirection(ushort[,] currentMatrix) public bool CalculateRightDirection() public bool CalculateLeftDirection() public bool CalculateUpDirection() public bool CalculateDownDirection()

--- GameTeamRain/GameTeamRain/GameCore.cs
+++ GameTeamRain/GameTeamRain/GameCore.cs
@@ -21,7 +21,7 @@
             
         }
 
-        public bool IsGameLost()
+        public bool IsGameOver()
         {
 
         }
@@ -36,29 +36,166 @@
 
         }
 
-        public bool ReCalculateMatrix(string direction)
+        public bool ReCalculateMatrix()
         {
+            bool isChangeCoreMatrix = false;
+            while (!isChangeCoreMatrix)
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                switch (input.Key.ToString())
+                {
+                    case "RightArrow": isChangeCoreMatrix = CalculateRightDirection();
+                        break;
+                    case "UpArrow": isChangeCoreMatrix = CalculateUpDirection();
+                        break;
+                    case "DownArrow": isChangeCoreMatrix = CalculateDownDirection();
+                        break;
+                    case "LeftArrow": isChangeCoreMatrix = CalculateLeftDirection();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return isChangeCoreMatrix;
+        }
+        public bool CalculateDirection(ushort[,] currentMatrix)
+        {
+            bool isChange = false;
+            for (int row = 0; row < currentMatrix.GetLength(0); row++)
+            {
+                int position = 0;
+                ushort currentCell = 0;
+                for (int col = 0; col < currentMatrix.GetLength(1) - 1; col++)
+                {
+                    if (currentMatrix[row, col] != 0)
+                    {
+                        for (int cell = col + 1; cell < currentMatrix.GetLength(1); cell++)
+                        {
+                            if ((currentMatrix[row, cell] != 0))
+                            {
+                                if ((currentMatrix[row, col] == currentMatrix[row, cell]))
+                                {
+                                    currentCell = (ushort)(currentMatrix[row, col] * 2);
+                                    currentMatrix[row, col] = 0;
+                                    currentMatrix[row, cell] = 0;
+                                    currentMatrix[row, position] = currentCell;
+                                    position++;
+                                    col = cell;
+                                    isChange = true;
+                                    break;
+                                }
+                                else
+                                {
+                                    currentCell = currentMatrix[row, col];
+                                    currentMatrix[row, col] = 0;
+                                    currentMatrix[row, position] = currentCell;
+                                    position++;
+                                    col = cell - 1;
+                                    isChange = true;
+                                    break;
+                                }
 
+                            }
+                        }
+                    }
+                }
+                if ((position == 0) && (currentMatrix[row, position] == 0))
+                {
+
+                    for (int j = 0; j < currentMatrix.GetLength(0); j++)
+                    {
+                        if (currentMatrix[row, j] != 0)
+                        {
+                            currentMatrix[row, position] = currentMatrix[row, j];
+                            currentMatrix[row, j] = 0;
+                            isChange = true;
+                            break;
+                        }
+                    }
+                }
+                if ((currentMatrix[row, currentMatrix.GetLength(1) - 2] == 0) && currentMatrix[row, currentMatrix.GetLength(1) - 1] != 0)
+                {
+                    currentMatrix[row, position] = currentMatrix[row, currentMatrix.GetLength(1) - 1];
+                    currentMatrix[row, currentMatrix.GetLength(1) - 1] = 0;
+                }
+            }
+            return isChange;
         }
         public bool CalculateRightDirection()
         {
+            ushort[,] currentMatrix = new ushort[this.coreMatrix.GetLength(0), this.coreMatrix.GetLength(1)];
+            for (int row = 0; row < this.coreMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.coreMatrix.GetLength(1); col++)
+                {
+                    currentMatrix[row, col] = this.coreMatrix[row, this.coreMatrix.GetLength(1) - col - 1];
+                }
+            }
 
+            bool isChange = CalculateDirection(currentMatrix);
+
+            for (int row = 0; row < this.coreMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.coreMatrix.GetLength(1); col++)
+                {
+                    this.coreMatrix[row, col] = currentMatrix[row, this.coreMatrix.GetLength(1)-col-1];
+                }
+            }
+            return isChange;
         }
 
         public bool CalculateLeftDirection()
         {
-
+           bool isChange = CalculateDirection(this.coreMatrix);
+           return isChange;
+            
         }
 
         public bool CalculateUpDirection()
         {
-        
+            ushort[,] currentMatrix = new ushort[this.coreMatrix.GetLength(0), this.coreMatrix.GetLength(1)];
+            for (int row = 0; row < this.coreMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.coreMatrix.GetLength(1); col++)
+                {
+                    currentMatrix[row,col]=this.coreMatrix[col,this.coreMatrix.GetLength(1)-row-1];
+                }
+            }
+            bool isChange = CalculateDirection(currentMatrix);
+
+            for (int row = 0; row < this.coreMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.coreMatrix.GetLength(1); col++)
+                {
+                    this.coreMatrix[col, this.coreMatrix.GetLength(1) - row - 1] =currentMatrix[row, col];
+                }
+            }
+
+            return isChange;
         }
 
         public bool CalculateDownDirection()
         {
+            ushort[,] currentMatrix = new ushort[this.coreMatrix.GetLength(0), this.coreMatrix.GetLength(1)];
 
+            for (int row = 0; row < this.coreMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.coreMatrix.GetLength(1); col++)
+                {
+                    currentMatrix[row, col] = this.coreMatrix[this.coreMatrix.GetLength(1) - col - 1,row];
+                }
+            }
+
+            bool isChange = CalculateDirection(currentMatrix);
+
+            for (int row = 0; row < this.coreMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.coreMatrix.GetLength(1); col++)
+                {
+                    this.coreMatrix[this.coreMatrix.GetLength(1) - col - 1, row] = currentMatrix[row, col];
+                }
+            }
+            return isChange;
         }
-
     }
 }
